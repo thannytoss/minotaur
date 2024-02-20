49d91a92ec5897added0b1d78220d9b67a50833f
Evan Fait <evanfait@Evans-Air.localdomain>
2024-02-20 16:09:39 -0600

    newest push with bootstrap changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider } from 'react-router-dom'
+import store from './store.js';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import HomeScreen from './screens/HomeScreen.jsx';
+import LoginScreen from './screens/LoginScreen.jsx';
+import RegisterScreen from './screens/RegisterScreen.jsx';
 
 //creating router
 // have to import actual components, don't forget </> brackets
@@ -12,12 +19,18 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path ='/' element={ <App /> }>
       <Route index={ true } path='/' element={ <HomeScreen /> } />
+      <Route path='/login' element={ <LoginScreen /> } />
+      <Route path='/register' element={ <RegisterScreen /> } />
+
+
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={ router } />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={ router } />
+    </React.StrictMode>,
+  </Provider>
 );
---
+++ frontend/src/screens/RegisterScreen.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col} from 'react-bootstrap'
+import FormContainer from "../components/formContainer";
+
+const RegisterScreen = () => {
+
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [confirmPassword, setConfirmPassword] = useState('');
+
+const [name, setName] = useState('');
+
+
+const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log('submit');
+}
+
+  return (
+    <FormContainer>
+        <h1>Sign Up</h1>
+
+        <Form onSubmit={ submitHandler }>
+        <Form.Group className="my-2" control='name'>
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                    type='text'
+                    placeholder='Enter Name'
+                    value={name}
+                    onChange={ (e) => setName(e.target.value) }
+                ></Form.Control>
+            </Form.Group>
+            <Form.Group className="my-2" control='email'>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                    type='email'
+                    placeholder='Enter Email'
+                    value={email}
+                    onChange={ (e) => setEmail(e.target.value) }
+                ></Form.Control>
+            </Form.Group>
+
+            <Form.Group className="my-2" control='password'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type='password'
+                    placeholder='Enter Password'
+                    value={password}
+                    onChange={ (e) => setPassword(e.target.value) }
+                ></Form.Control>
+            </Form.Group>
+            <Form.Group className="my-2" control='confirmPassword'>
+                
+                <Form.Control
+                    type='password'
+                    placeholder='Confirm Password'
+                    value={confirmPassword}
+                    onChange={ (e) => setConfirmPassword(e.target.value) }
+                ></Form.Control>
+            </Form.Group>
+            <Button type ='submit' variant="primary" className="mt-3">
+                Sign Up
+            </Button>
+
+            <Row className="py-3">
+                <Col>
+                 Already a user? <Link to='/login'>Sign In!</Link>
+                </Col>
+            </Row>
+        </Form>
+    </FormContainer>
+  )
+}
+
+export default RegisterScreen
